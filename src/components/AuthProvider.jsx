e9b2f98e07def7fbe553b85ddd2beea019665a6e
Mafikul Islam <mdmafikulislam5320@gmail.com>
2025-04-30 11:56:21 +0600

    Set ContextApI Any Components use Value

--- src/components/AuthProvider.jsx
+++ src/components/AuthProvider.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
 
+import { auth } from './LoginReg';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+
 const AuthProvider = ({ children }) => {
-    const UserEmail = {
-        email: "abc@gmail.com"
+
+    const createUser = (Email, Password) => {
+        return createUserWithEmailAndPassword(auth, Email, Password)
+    }
+    const User = {
+        createUser
     }
     return (
-        <AuthContext value={UserEmail}>
+        <AuthContext value={User}>
             {children}
         </AuthContext>
     );
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,16 +1,20 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import React from 'react';
+// import { createUserWithEmailAndPassword } from 'firebase/auth';
+import React, { use } from 'react';
 import { Link } from 'react-router';
-import { auth } from './LoginReg';
+import { AuthContext } from './AuthContext';
+// import { auth } from './LoginReg';
 
 const Register = () => {
+    const CreateNewUser = use(AuthContext);
+    const { createUser } = (CreateNewUser)
     const HandelRegister = (event) => {
         event.preventDefault()
         const Name = event.target.name.value;
         const Email = event.target.email.value;
         const Password = event.target.Password.value;
         console.log(Name, Email, Password)
-        createUserWithEmailAndPassword(auth, Email, Password).then(res => console.log(res)).catch(error => console.log(error))
+        createUser(Email, Password).then(res => console.log(res)).catch(error => error.message)
+        // createUserWithEmailAndPassword(auth, Email, Password).then(res => console.log(res)).catch(error => console.log(error))
     }
     return (
         <div className="card bg-base-100 w-full mx-auto my-18 max-w-sm shrink-0 shadow-2xl">
